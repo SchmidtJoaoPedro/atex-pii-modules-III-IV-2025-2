d025899d524594d9f2f7ac46e37a6199b5bb21aa
RafaelEngel10 <rafaelengelserafim09@gmail.com>
2025-09-05 20:04:20 -0300

    arquivo inutil excluido

--- JS/animate.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const cadcontainer = document.getElementById("cadcontainer");
-    cadcontainer.classList.add("reveal");
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    const logcontainer = document.getElementById("logcontainer");
-    logcontainer.classList.add("reveal");
-});
